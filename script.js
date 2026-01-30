5e06990faa3176c855ecdcf46cdce19983b103f9
PremCodeWorld <shahprem920@gmail.com>
2026-01-30 08:39:28 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Simple smooth scroll animation
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute("href"))
+            .scrollIntoView({ behavior: "smooth" });
+    });
+);
+
+// Welcome message
+console.log("Welcome to Chandan Tent House website!");
